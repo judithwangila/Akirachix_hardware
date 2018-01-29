a39ada0ce43c95f18adb6b044df8f6330d8d1c33
judith wangila <judithnwangila@gmail.com>
2018-01-29 13:46:24 +0300

    first commit

---
+++ judith_cpp.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+int addition(int x,int y){int z = y=x;
+return z;
+}    
+
+int main()
+{int a;
+int b;
+cout<<"3";
+cin>>b;
+cout<<"4";
+cin>>b;
+cout<<a;cout<<" + ";cout<<b;cout<<" = ";cout<<addition(a,b);
+    
+
+    return 0;
+}
+
+
+
